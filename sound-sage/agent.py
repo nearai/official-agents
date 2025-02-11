4909985fa1455e762347783b2811e5bd7231b269
Gabe Hamilton <gabe@near.ai>
2025-02-11 11:08:35 -0700

    sound-sage (#13)
    * agent-tests 0.0.1 produces valid (draft) request_decision message
    
    * initial soundsage agent files

---
+++ sound-sage/agent.py
@@ -0,0 +1,36 @@
+# This agent helps users buy audio equipment.
+import json
+import prompt
+import shopping_mcp
+import products_aitp
+import payments_aitp
+import checkout_aitp
+
+class Agent:
+    def __init__(self, env):
+        self.env = env
+
+    def request_decision_test(self, user_message):
+        env = self.env
+
+        env.add_reply(env.completion(
+            [{"role": "system", "content": prompt.PROMPT},
+             {"role": "system", "content": json.dumps(prompt.example_product_data)},
+             {"role": "user", "content": user_message}],
+        ))
+
+    def run(self):
+        try:
+            env = self.env
+            user_message = env.get_last_message()
+            self.request_decision_test(user_message)
+        except Exception as e:
+            # print stacktrace
+            import traceback
+            traceback.print_exc()
+            self.env.add_reply(f"Error: {e}")
+
+
+if globals().get('env', None):
+    agent = Agent(globals().get('env', {}))
+    agent.run()
---
+++ sound-sage/products_aitp.py
@@ -0,0 +1,12 @@
+class ProductsAITP:
+    def __init__(self, env):
+        self.env = env
+
+    def generate_request_decision(self, product_data):
+        pass
+
+    def populate_quotes(self, product_data):
+        pass
+
+    def validate_aitp_message(self, message):
+        pass
